1b612ae938dbcb7ebe9b345f77559ba0aa60fb34
Jules <julialrebello@utexas.edu>
2018-10-27 11:47:29 -0500

    initialize

---
+++ Header.java
@@ -0,0 +1,15 @@
+package assignment4;
+/* CRITTERS Critter.java
+ * EE422C Project 4 submission by
+ * Julia Rebello
+ * JLR3755
+ * Section 16365
+ * Samir Riad
+ * SR43888
+ * 16360
+ * Slip days used: <0>
+ * Fall 2018
+ */
+public class Header {
+
+}
