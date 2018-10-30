85ca22e758b590f5fb249dd2956ae9a32b948e7a
Jules <julialrebello@utexas.edu>
2018-10-29 23:47:26 -0500

    Update CritterWorld.java

--- CritterWorld.java
+++ CritterWorld.java
@@ -14,7 +14,10 @@ import java.util.ArrayList;
 
 public class CritterWorld {
     public static ArrayList[][] world;
-
+    /**
+     * CritterWorld method
+     * This method creates our simulated world.
+     */
     public CritterWorld() {
         world = new ArrayList[Params.world_height][Params.world_width];
         for(int i = 0; i < Params.world_height; i++) {
@@ -23,7 +26,4 @@ public class CritterWorld {
             }
         }
     }
-
-
-    // write methods for getting char symbols for whatever critter is accessed
 }
